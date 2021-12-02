3bffb88bba9719cf355e1b72126f424faaf60c7d
Alex Gerdisch <alexgerdisch@gmail.com>
2021-12-01 20:13:52 -0800

    first commit

---
+++ src/routes/resources.svelte
@@ -0,0 +1,37 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="content">
+	<h1>Resource Portal</h1>
+	<p>UNDER CONSTRUCTION</p>
+	<p>To be added:</p>
+	<p>Common resource requests (SOC2, PCI, etc.)</p>
+	<p>Onboarding docs</p>
+	<p>Guides</p>
+	
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
